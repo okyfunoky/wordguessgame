1acfa588b73657cd1ec06d2d372a2db51c7970bc
Okyfunoky <okyfunoky@hotmail.com>
2019-08-28 15:39:44 -0700

    Function layout

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,58 @@
 var game = {
-    guesses = 0,
-    totalGuesses = 10,
-    hasWon = false,
-    movies =[
-        { name: "Avengers", backgroundLink: "foo" },
-        { name: "", backgroundLink: "" },
-        { name: "", backgroundLink: "" },
-        { name: "", backgroundLink: "" },
-        { name: "", backgroundLink: "" },
-        { name: "", backgroundLink: "" },
+    guesses: 0,
+    totalGuesses: 10,
+    hasWon: false,
+    movies: [
+        { name: "Avengers", backgroundLink: "foo", hint: "", selected: false },
+        { name: "Captain America: The First Avenger", backgroundLink: "", hint: "", selected: false },
+        { name: "Thor", backgroundLink: "", hint: "", selected: false },
+        { name: "Thor: The Dark World", backgroundLink: "", hint: "", selected: false },
+        { name: "Thor: Ragnarock", backgroundLink: "", hint: "", selected: false },
+        { name: "Iron Man", backgroundLink: "", hint: "", selected: false },
     ]
 
 }
 
-var marvelMovie = {
-    name,
-    backgroundLink
+console.log("Logging name " + game.movies[0].name);
+console.log("Logging link " + game.movies[0].backgroundLink);
+
+document.onkeyup = function(event) {
+    playGame();
 }
-
-
 
 function playGame() {
     //TODO: Choose a movie
-    //TODO: Update the html with the correct blanks
-    //TODO: Accept user input
+    chooseMovie();
     //TODO: Check if chosen letter is in movie
     //TODO: If in, update HTML
     //TODO: If not, increment wrong counter
     //If out of guesses, fail
 
+};
+
+function chooseMovie() {
+    var randomNumber = Math.floor(Math.random() * game.movies.length);
+    console.log(randomNumber)
 
 
+    if(!game.movies[randomNumber].selected){
+        game.movies[randomNumber].selected = true;
+        resetGame(game.movies[randomNumber]);
+    }
+    else{
+        chooseMovie();
+    }
+}; 
+
+function resetGame(movie) {
+    game.guesses = 0;
+    var guessesRemaining = document.getElementById("guessesRemaining");
+    guessesRemaining.textContent = game.totalGuesses;
+    var movieToGuess = document.getElementById("wordToGuess");
+    movieToGuess.textContent = movie.name;
+    //TODO: Empty existing spans
+    //TODO: Update spans with reset values
+    //TODO: Reset values (guesses)
 };
 
 function updateGuessedLetters() {
@@ -38,6 +60,10 @@ function updateGuessedLetters() {
 };
 
 function updateSolution() {
+
+};
+
+function incrementWrongCounter(){
 
 };
 
