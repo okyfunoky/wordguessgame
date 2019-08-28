ae938b785683ed0652d149a96e3e69e350c19adc
Okyfunoky <okyfunoky@hotmail.com>
2019-08-28 12:39:46 -0700

    Sketching out js

---
+++ assets/javascript/game.js
@@ -0,0 +1,46 @@
+var game = {
+    guesses = 0,
+    totalGuesses = 10,
+    hasWon = false,
+    movies =[
+        { name: "Avengers", backgroundLink: "foo" },
+        { name: "", backgroundLink: "" },
+        { name: "", backgroundLink: "" },
+        { name: "", backgroundLink: "" },
+        { name: "", backgroundLink: "" },
+        { name: "", backgroundLink: "" },
+    ]
+
+}
+
+var marvelMovie = {
+    name,
+    backgroundLink
+}
+
+
+
+function playGame() {
+    //TODO: Choose a movie
+    //TODO: Update the html with the correct blanks
+    //TODO: Accept user input
+    //TODO: Check if chosen letter is in movie
+    //TODO: If in, update HTML
+    //TODO: If not, increment wrong counter
+    //If out of guesses, fail
+
+
+
+};
+
+function updateGuessedLetters() {
+
+};
+
+function updateSolution() {
+
+};
+
+function checkForVictory() {
+
+};
