04f55f873938f22179b201531dd1ab3986fa91c1
Michael Stevens <okyfunoky@hotmail.com>
2019-08-29 08:05:16 -0700

    Removing unused objects

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,67 +1,6 @@
 console.log("start");
 
 var characters = ["Captain America", "Black Panther","Iron Man","Thor","Black Widow","Hawkeye","Doctor Strange","The Wasp","Spider-Man","Star-Lord","Captain Marvel", "Valkyrie"]
-// var pictures = [
-//     {name: "captainamerica", url: "captainamerica.jpg"},
-//     {name: "blackpanther", url: ""},
-//     {name: "ironman", url: "ironman.jpg"},
-//     {name: "thor", url: "thor.jfif"},
-//     {name: "blackwidow", url: "blackwidow.jpg"},
-//     {name: "hawkeye", url: "hawkeye.webp"},
-//     {name: "doctorstrange", url: "doctorstrange.png"},
-//     {name: "thewasp", url: ""},
-//     {name: "spiderman", url: "spiderman.jpg"},
-//     {name: "starlord", url: ""},
-//     {name: "captainmarvel", url: ""},
-//     {name: "valkyrie", url: ""},
-// ]
-
-// var game = {
-//     captainamerica: {
-//         name: "Captain America",
-//         url: "captainamerica.jpg",
-//     },
-//     ironman: {
-//         name: "Iron Man",
-//         url: "ironman.jph"
-//     },
-//     blackpanther: {
-//         name: "Black Panther",
-//         url: ""
-//     },
-//     thor: {
-//         name: "Thor",
-//         url: "thor.jfif"
-//     },
-//     blackwidow: {
-//         name: "Black Widow",
-//         url: "blackwidow.jpg"
-//     },
-//     hawkeye: {
-//         name: "Hawkeye",
-//         url: "hawkeye.webp"
-//     },
-//     doctorstrange: {
-//         name: "Doctor Strange",
-//         url: "doctorstrange.png"
-//     },
-//     spiderman: {
-//         name: "Spider-Man",
-//         url: "spiderman.jpg"
-//     },
-//     starlord: {
-//         name: "Star-Lord",
-//         url: ""
-//     },
-//     captainmarvel: {
-//         name: "Captain Marvel",
-//         url: ""
-//     },
-//     valkyrie: {
-//         name: "Valkyrie",
-//         url: ""
-//     }
-// }
 
 var character = characters[11];
 var guesses = 5;
