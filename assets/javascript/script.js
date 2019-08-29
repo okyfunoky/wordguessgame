b503e7cbefe7d992b12c3cf257a04da51d389d73
Michael Stevens <okyfunoky@hotmail.com>
2019-08-29 08:04:29 -0700

    Fully functioning, just needs style

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,68 @@
+console.log("start");
+
 var characters = ["Captain America", "Black Panther","Iron Man","Thor","Black Widow","Hawkeye","Doctor Strange","The Wasp","Spider-Man","Star-Lord","Captain Marvel", "Valkyrie"]
+// var pictures = [
+//     {name: "captainamerica", url: "captainamerica.jpg"},
+//     {name: "blackpanther", url: ""},
+//     {name: "ironman", url: "ironman.jpg"},
+//     {name: "thor", url: "thor.jfif"},
+//     {name: "blackwidow", url: "blackwidow.jpg"},
+//     {name: "hawkeye", url: "hawkeye.webp"},
+//     {name: "doctorstrange", url: "doctorstrange.png"},
+//     {name: "thewasp", url: ""},
+//     {name: "spiderman", url: "spiderman.jpg"},
+//     {name: "starlord", url: ""},
+//     {name: "captainmarvel", url: ""},
+//     {name: "valkyrie", url: ""},
+// ]
+
+// var game = {
+//     captainamerica: {
+//         name: "Captain America",
+//         url: "captainamerica.jpg",
+//     },
+//     ironman: {
+//         name: "Iron Man",
+//         url: "ironman.jph"
+//     },
+//     blackpanther: {
+//         name: "Black Panther",
+//         url: ""
+//     },
+//     thor: {
+//         name: "Thor",
+//         url: "thor.jfif"
+//     },
+//     blackwidow: {
+//         name: "Black Widow",
+//         url: "blackwidow.jpg"
+//     },
+//     hawkeye: {
+//         name: "Hawkeye",
+//         url: "hawkeye.webp"
+//     },
+//     doctorstrange: {
+//         name: "Doctor Strange",
+//         url: "doctorstrange.png"
+//     },
+//     spiderman: {
+//         name: "Spider-Man",
+//         url: "spiderman.jpg"
+//     },
+//     starlord: {
+//         name: "Star-Lord",
+//         url: ""
+//     },
+//     captainmarvel: {
+//         name: "Captain Marvel",
+//         url: ""
+//     },
+//     valkyrie: {
+//         name: "Valkyrie",
+//         url: ""
+//     }
+// }
+
 var character = characters[11];
 var guesses = 5;
 var placeholderArray = createPlaceholderArray(character);
@@ -7,7 +71,6 @@ var guessesRemaining = document.getElementById("guessesRemaining");
 guessesRemaining.textContent = guesses;
 
 document.onkeyup = function(event){
-
 
     keyPressed = event.key;
     character = character.toLowerCase();
@@ -29,7 +92,7 @@ document.onkeyup = function(event){
 
     updateWordToGuess(placeholderArray);
     var result = checkForWinLoss();
-    updateHeroImage(result);
+    updateHeroImage(result, character);
 }
 
 function updateWordToGuess(update){
@@ -70,7 +133,16 @@ function checkForWinLoss(){
 
 function updateHeroImage(result, character){
     if(result === "win"){
-        //find picture in collection
+        console.log(character);
+        character = character.replace("-","");
+        character = character.replace(" ","");
+        console.log(character)
+        var pictureUrl = character + ".jpg"
+        
+        console.log("url " + pictureUrl);
+        var background = document.getElementById("heroImage");
+        background.style = "background-image: url('./assets/images/" + pictureUrl + "'); background-repeat:no-repeat;";
+
     }else if(result === "lose"){
         //thanos
         var background = document.getElementById("heroImage");
