5c4ed4de97d40623b6872f9a7271dc49adb10abc
Michael Stevens <okyfunoky@hotmail.com>
2019-08-28 19:56:01 -0700

    basic, functioning game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 var game = {
     guesses: 0,
     totalGuesses: 10,
-    hasWon: false,
+    isOver: false,
     movies: [
         { name: "Avengers", backgroundLink: "foo", hint: "", selected: false },
         { name: "Captain America: The First Avenger", backgroundLink: "", hint: "", selected: false },
@@ -9,47 +9,97 @@ var game = {
         { name: "Thor: The Dark World", backgroundLink: "", hint: "", selected: false },
         { name: "Thor: Ragnarock", backgroundLink: "", hint: "", selected: false },
         { name: "Iron Man", backgroundLink: "", hint: "", selected: false },
-    ]
-
+    ],
+    guessedLetters: [],
 }
 
 console.log("Logging name " + game.movies[0].name);
 console.log("Logging link " + game.movies[0].backgroundLink);
 
-document.onkeyup = function(event) {
-    playGame();
-}
+playGame();
+
 
 function playGame() {
     //TODO: Choose a movie
-    chooseMovie();
+    //chooseMovie();
     //TODO: Check if chosen letter is in movie
     //TODO: If in, update HTML
     //TODO: If not, increment wrong counter
     //If out of guesses, fail
+    currentMovie = game.movies[1]
+    var placeholder = resetGame(currentMovie);
 
+    document.onkeyup = function(event){
+        var keyPressed = event.key;
+
+        if(!game.guessedLetters.includes(keyPressed)){
+            game.guessedLetters.push(keyPressed);
+        }
+
+        currentName = currentMovie.name.toLowerCase();
+        console.log("Current Name:" + currentName);
+        console.log("Key Pressed " + keyPressed);
+
+        if(currentName.includes(keyPressed)) {
+            console.log("Got into loop");
+            for (let index = 0; index < currentName.length; index++) {
+
+                if(keyPressed === currentName[index]){
+                    console.log("Placeholder before:" + placeholder)
+                    placeholder[index] = keyPressed;
+                    console.log("Placeholder after:" + placeholder)
+                    var movieToGuess = document.getElementById("wordToGuess");
+                    movieToGuess.textContent = placeholder;
+                }   
+                
+            }
+        }
+
+
+        else{
+            document.getElementById("guessedLetters").textContent += " " + keyPressed;
+            game.guesses++;
+            if(game.guesses > game.totalGuesses){
+                
+            }
+        }
+    }
 };
 
 function chooseMovie() {
     var randomNumber = Math.floor(Math.random() * game.movies.length);
     console.log(randomNumber)
 
-
     if(!game.movies[randomNumber].selected){
         game.movies[randomNumber].selected = true;
         resetGame(game.movies[randomNumber]);
     }
-    else{
+    else {
         chooseMovie();
     }
 }; 
 
 function resetGame(movie) {
+    console.log(movie);
     game.guesses = 0;
     var guessesRemaining = document.getElementById("guessesRemaining");
     guessesRemaining.textContent = game.totalGuesses;
+    var placeholder = ""
+    
+    for (let index = 0; index < movie.name.length; index++) {
+        console.log(movie.name[index])
+        if(movie.name[index] !== ' ' && movie.name[index] !== ':' ){
+            placeholder += "_"
+        }else{
+            placeholder += movie.name[index];
+        }
+
     var movieToGuess = document.getElementById("wordToGuess");
-    movieToGuess.textContent = movie.name;
+    console.log(placeholder);
+    movieToGuess.textContent = placeholder;
+    return placeholder;
+}
+
     //TODO: Empty existing spans
     //TODO: Update spans with reset values
     //TODO: Reset values (guesses)
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,29 +1,64 @@
 var character = "Captain America"
-var gameDisplay = "";
-//should programatically set this to update the html
 var guesses = 5;
-
+var placeholderArray = createPlaceholderArray(character);
+updateWordToGuess(placeholderArray);
+var guessesRemaining = document.getElementById("guessesRemaining");
+guessesRemaining.textContent = guesses;
 
 document.onkeyup = function(event){
     keyPressed = event.key;
     character = character.toLowerCase();
 
     if(character.includes(keyPressed)){
-        var guessedLetters = document.getElementById("guessedLetters");
-        guessedLetters.textContent += keyPressed;
-
         for (let i = 0; i < character.length; i++) {
             if(character[i] === keyPressed){
-                gameDisplay[i] = keyPressed;
+                placeholderArray[i] = keyPressed;
+                console.log("setting value: " + placeholderArray[i] + "to key");
+                console.log("GameDisplay variable: " + placeholderArray);
             }
         }
-    }else{
-        var guessesRemaining = document.getElementById("guessesRemaining");
-        guessesRemaining.textContent = guesses--;
+    }else {
+        guesses--;
+        guessesRemaining.textContent = guesses;
     }
+    var guessedLetters = document.getElementById("guessedLetters");
+    guessedLetters.textContent += keyPressed;
 
-    var result = document.getElementById("wordToGuess");
-    result.textContent = gameDisplay;
-
+    updateWordToGuess(placeholderArray);
+    var result = checkForWinLoss();
+    console.log(result);
 }
 
+function updateWordToGuess(update){
+    var result = document.getElementById("wordToGuess");
+    var updateString = "";
+
+    for (let i = 0; i < update.length; i++) {
+        updateString += update[i];
+    }
+
+    result.textContent = updateString;
+}
+
+function createPlaceholderArray(character){
+    var placeholderArray = [];
+    for (let i = 0; i < character.length; i++) {
+        if(character[i]!== " ")
+        {
+            placeholderArray.push("_");
+        }else{
+            placeholderArray.push(" ");
+        }
+    }
+    return placeholderArray;
+}
+
+function checkForWinLoss(){
+    if(guesses <= 0){
+        return "lose";
+    }
+
+    if(placeholderArray.indexOf("_") === -1){
+        return "Win";
+    }
+}
